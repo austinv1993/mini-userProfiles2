9da5fc27e82bc5d10c7ccd4d68ae0d2deb7e97d2
Austin Vance <austinvance@AustinVancesMBP.home>
2015-10-28 13:03:22 -0600

    updated

--- controller.js
+++ controller.js
@@ -2,11 +2,11 @@ var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    $scope.getUsers = function() {
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
   }
-
-
   $scope.getUsers();
 
 });
